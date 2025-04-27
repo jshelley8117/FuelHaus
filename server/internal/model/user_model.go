35f637aaafd6fcfcb289febb103c294bc3d26af4
jshelley8117 <jshelley8117@gmail.com>
2025-04-27 15:53:11 -0500

    refactor: modify user model

--- server/internal/model/user_model.go
+++ server/internal/model/user_model.go
@@ -3,12 +3,12 @@ package model
 import "time"
 
 type User struct {
-	UserId    string    `firestore:"userId"`
-	FirstName string    `firestore:"first_name" validate:"required"`
-	LastName  string    `firestore:"last_name" validate:"required"`
-	Email     string    `firestore:"email" validate:"required,email"`
-	Password  string    `firestore:"password" validate:"required"`
-	CreatedAt time.Time `firestore:"created_at"`
-	UpdatedAt time.Time `firestore:"updated_at"`
-	Is_Active bool      `firestore:"is_active"`
+	UserId       string    `firestore:"userId"`
+	FirstName    string    `firestore:"first_name" validate:"required"`
+	LastName     string    `firestore:"last_name" validate:"required"`
+	Email        string    `firestore:"email" validate:"required,email"`
+	Password     string    `firestore:"password" validate:"required"`
+	CreatedAt    time.Time `firestore:"created_at"`
+	UpdatedAt    time.Time `firestore:"updated_at"`
+	IsUserActive bool      `firestore:"is_active"`
 }
