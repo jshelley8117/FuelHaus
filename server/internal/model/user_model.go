8583dfebd3925e9def180407ed8789898b8d7649
jshelley8117 <jshelley8117@gmail.com>
2025-04-27 13:03:15 -0500

    refactor: update go user model to reflect firestore user collection

--- server/internal/model/user_model.go
+++ server/internal/model/user_model.go
@@ -4,9 +4,11 @@ import "time"
 
 type User struct {
 	UserId       uint16    `json:"userId,omitempty"`
+	FirstName    string    `json:"first_name" validate:"required"`
+	LastName     string    `json:"last_name" validate:"required"`
 	Email        string    `json:"email" validate:"required,email"`
 	Password     string    `json:"password" validate:"required"`
-	CreatedAt    time.Time `json:"createdAt,omitempty"`
-	UpdatedAt    time.Time `json:"updatedAt,omitempty"`
-	IsUserActive bool      `json:"isUserActive,omitempty"`
+	CreatedAt    time.Time `json:"created_at,omitempty"`
+	UpdatedAt    time.Time `json:"updated_at,omitempty"`
+	IsUserActive bool      `json:"is_active,omitempty"`
 }
